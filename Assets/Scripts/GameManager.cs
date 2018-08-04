3fb5677744dcc063cd4b8a29292f66c1bd61f4ee
Ryan Cherry <cherryryan@ecs.vuw.ac.nz>
2018-08-04 17:13:25 +1200

    Added script for walls

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,18 @@ using System.Collections.Generic;
     
 public class GameManager : MonoBehaviour {
 
+    public static GameManager instance = null;
+
     private BoardManager boardScript;
     private int level = 3;
 
     void Awake() {
+        if (instance == null){
+            instance = this;
+        } else if (instance != this){
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
         boardScript = GetComponent<BoardManager>();
         InitGame();
     }
